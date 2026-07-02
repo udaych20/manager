aa10ad9e8d78fc5a9e8421985d015558e56c0752
Uday Chintala <udaych20@gmail.com>
2026-07-02 13:07:20 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
-:host ::ng-deep .ternaryLevelP > li.active > a,
-:host ::ng-deep .ternaryLevelP > li.active > a.nav-link,
-:host ::ng-deep .ternaryLevelP > li > a.active,
-:host ::ng-deep .ternaryLevelP > li > a[aria-selected="true"] {
-  background-color: #336699 !important;
+.ternaryLevelP > li.active > a,
+.ternaryLevelP > li.active > a.nav-link,
+.ternaryLevelP > li > a.active,
+.ternaryLevelP > li > a[aria-selected="true"] {
+  background-color: #3a75af !important;
   color: #ffffff !important;
 }
